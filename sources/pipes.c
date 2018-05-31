aed15a1d6756dbdbcde27e7d550b93d057937269
lezanaDcristian <39574723+lezanaDcristian@users.noreply.github.com>
2018-05-31 00:06:46 -0400

    Update pipes.c
    Se agrego la comunicación de la generación de tiempos y las listas random

--- sources/pipes.c
+++ sources/pipes.c
@@ -6,17 +6,28 @@ int main(int argc,char **argv){
 	pid_t pid[TOTAL_CHILDS];
 	int status[TOTAL_CHILDS];
 	int fd[TOTAL_CHILDS][2];
+	int fdwait[TOTAL_CHILDS][2];
+	int fdlist[TOTAL_CHILDS][2];
 	int wait_time[TOTAL_CHILDS];
 	int *random_list[TOTAL_CHILDS];
+	int list_wait_buffer[TOTAL_CHILDS];
+	int list_random_buffer[TOTAL_CHILDS][TOTAL_NUMBERS];
+
 	/* Inicialización de parámetros (pipes, wait_time, random_list[child]) */
 	int child;
 	for(child=0;child<TOTAL_CHILDS;child++){
-		if(pipe(fd[child])==-1){
+		if(pipe(fd[child])==-1 && pipe(fdfa[child])==-1){
 			fprintf(stderr,"Pipe failed\n");
 			return 1;
 		}
 		wait_time[child]=MIN_WAIT_TIME+random()%(MAX_WAIT_TIME-MIN_WAIT_TIME);
 		random_list[child]=randomList(TOTAL_NUMBERS,MIN_RANDOM_NUMBER,MAX_RANDOM_NUMBER);
+		close(fdwait[child][READ_END]);
+		write(fdwait[child][WRITE_END],wait_time[child],sizeof(int));
+		close(fdwait[child][WRITE_END])
+		close(fdlist[child][READ_END]);
+		write(fdlist[child][WRITE_END],random_list[child],sizeof(int)*TOTAL_NUMBERS);
+		close(fdlist[child][WRITE_END]);
 	}
 	/* Generación de procesos hijos */
 	for(child=0;child<TOTAL_CHILDS;child++){
@@ -26,7 +37,13 @@ int main(int argc,char **argv){
 			return 1;
 		}
 		if(pid[child]==0){//proceso hijo child-ésimo
-			childProcessPipe(random_list[child],wait_time[child],fd[child]);
+			close(fdwait[child][WRITE_END]);
+			read(fdwait[child][READ_END],list_wait_buffer[child], sizeof(int));
+			close(fdwait[child][READ_END]);
+			close(fdlist[child][WRITE_END]);
+			read(fdlist[child][READ_END]),list_random_buffer[child],sizeof(int)*TOTAL_NUMBERS;
+			close(fdlist[child][READ_END]);
+			childProcessPipe(list_random_buffer[child],list_wait_buffer[child],fd[child]);
 			return 0;
 		}
 	}
